c6d8e4d8c778c8064edf093ed61f7cc42072176d
NoranMohammed <noranmohammed2310@gmail.com>
2024-07-29 17:33:41 +0300

    House Cards done

--- src/components/dashboard/HouseCard.jsx
+++ src/components/dashboard/HouseCard.jsx
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Divider, IconButton, Stack, Typography } from '@mui/material';
+import { blue, grey } from '@mui/material/colors';
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
+import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
+import BedRoundedIcon from '@mui/icons-material/BedRounded';
+import BathtubRoundedIcon from '@mui/icons-material/BathtubRounded';
+import AspectRatioRoundedIcon from '@mui/icons-material/AspectRatioRounded';
+import { useSelector } from 'react-redux';
+import LoadingPage from '../../pages/LoadingPage';
+import { Link } from 'react-router-dom';
 
-function HouseCard() {
-  return <div>HouseCard</div>;
+function HouseCard(props) {
+  
+  const house=props.data
+  const [like,setlike]=useState(false)
+   const handleIcon=()=>{
+     setlike(!like)
+   }
+   
+   if(!house){
+    return <LoadingPage></LoadingPage>
+   }
+  return <>
+   <Card
+    sx={{
+      width: '300px',
+      margin:"10px",
+      '&:hover': {
+       
+        boxShadow: `5px 5px 24px ${grey[300]}`,
+      },
+    }}
+   >
+    <CardMedia
+     component={"img"}
+     alt={"House"}
+     height={"200px"}
+     image={house.imageUrl}
+    />
+    <CardContent sx={{display:'flex', flexDirection:"column",gap:"5"}}>
+         <Stack direction={"row"} justifyContent={'space-between'}>           
+            <Typography variant='P' color={blue[900]}><b>{house.price}$</b></Typography>
+            <IconButton onClick={handleIcon}>
+              {like?<FavoriteRoundedIcon color='error'/>: <FavoriteBorderRoundedIcon/>}
+            </IconButton> 
+        </Stack>
+      <Typography  variant='p' >{house.title}</Typography>
+      <Stack direction={'row'} gap={2} sx={{color:grey[700],fontSize:"10pt",marginTop:"15px"}}>
+        
+        <Stack direction={"row"} gap={1}>
+          <BedRoundedIcon sx={{fontSize:"15pt"}} />
+          <Typography  variant='p'>{house.bedrooms} beds</Typography>
+        </Stack>
+        <Stack direction={"row"} gap={1}>
+          <BathtubRoundedIcon sx={{fontSize:"15pt"}}/>
+          <Typography  variant='p'>{house.bathrooms} Baths</Typography>
+        </Stack>
+        <Stack direction={"row"} gap={1}>
+          <AspectRatioRoundedIcon sx={{fontSize:"15pt"}} />
+          <Typography  variant='p' >{house.area} SqFt</Typography>
+        </Stack>
+      </Stack>
+    </CardContent>
+    <CardActions>
+      <Button variant="contained" color='info' sx={{borderRadius:"15px"}} component={Link} to={`houses/${house._id}`}>More info</Button>
+    </CardActions>
+    
+
+   </Card>
+  
+  
+  
+  
+  </>
+  ;
 }
 
 export default HouseCard;
--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -1,20 +1,47 @@
 import React, { useEffect } from 'react';
 import FilterSection from '../../components/dashboard/FilterSection';
 import TopPremium from '../../components/dashboard/TopPremium';
+import{getTopRatedRentHousesService,getTopRatedSellHousesService}  from '../../services/houseService'
+import { useDispatch, useSelector } from 'react-redux';
+import { housesActions} from '../../store/houses-slice'
 function Home() {
   // fetch data
-  useEffect(() => {
-    // fetch data for top premium for rent
-    // fetch data for top premium for
-  }, []);
+  const dispatch=useDispatch();
+  const houses=useSelector(state=>state.houses.houses)
+  const RentHouses=useSelector(state=>state.houses.topRatedRentals)
+  const Sellhouses=useSelector(state=>state.houses.topRatedBuys)
+
+ 
+  useEffect(function() {
+    async function fetchTopRatedHouses() {
+     
+        const rentResult = await getTopRatedRentHousesService();
+       dispatch(housesActions.setTopRatedRentals(rentResult.data));
+      const sellResult = await getTopRatedSellHousesService();
+      dispatch(housesActions.setTopRatedBuys(sellResult.data));
+    }
+  
+    fetchTopRatedHouses();
+  }, [dispatch]);
+  // console.log(houses);
+  // useEffect( async() => { 
+  //   const GetTopRent= await getTopRatedRentHousesService();
+  //    const GetTopSell=await getTopRatedSellHousesService();
+  //    dispatch( housesActions.setTopRatedRentals(GetTopRent.data))
+  //    dispatch(housesActions.setTopRatedBuys(GetTopSell.data))
+     
+  //   // fetch data for top premium for rent
+  //   // fetch data for top premium for
+  // }, [dispatch]);
+  
   return (
     <div>
       {/* preview section */}
       <FilterSection />
       {/* premium top for rent */}
-      <TopPremium title='Top Premium for Rent' data={[]} />
+      <TopPremium title='Top Premium for Rent' data={RentHouses} />
       {/* premium top for sale */}
-      <TopPremium title='Top Premium for Sale' data={[]} />
+      <TopPremium title='Top Premium for Sale' data={Sellhouses} />
     </div>
   );
 }
